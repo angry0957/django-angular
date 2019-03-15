8ab91c62d82fb0b090bb6462680cc841fb68deb2
Abdul Rahman <abdul.095722@gmail.com>
2019-03-16 00:28:40 +0500

    login

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -15,14 +15,19 @@ import * as jwt_decode from "jwt-decode";
 export class LoginComponent implements OnInit {
 	username;
 	password;
-	url = "http://localhost:8000/auth-jwt/";
+	url = "http://localhost:8000/loginUser/";
 
 	constructor(private http:HttpClient, private router: Router, private authService: AuthService) { }
 
 	ngOnInit() {
-		this.authService.verifyUser('ads').subscribe((data)=> {
+		this.authService.verifyUser('ads').subscribe((data:any)=> {
+			if(data.type == "lawyer")
+			{
+				this.router.navigate(['/editprofile-lawyer']);
+			}
 			console.log("Verify success",data);
 			this.router.navigate(['/home']);
+
 		},
 		(err) => {
 		});
@@ -33,31 +38,32 @@ export class LoginComponent implements OnInit {
 	}
 
 	onSubmit(f: NgForm) {
-		console.log(f.value.username,f.value.password);
 		this.username = f.value.username;
 		this.password = f.value.password;
-		console.log(f.valid);
-		let obj ={
-			'Accept': 'application/json',
-			username: this.username,
-			password: this.password
-		}
+		let formdata = new FormData();
+		formdata.append('username', f.value.username);
+		formdata.append('password', f.value.password);
 
-		let headers = new HttpHeaders({
-			'Accept': 'application/json',
-		});
-
-		this.http.post(this.url,obj).toPromise().then((res:any) => {
+		console.log('Inside ',this.username,this.password)
+		
+		this.http.post(this.url,formdata).toPromise().then((res:any) => {
 			console.log('Response',res);
+			localStorage.setItem('token',res.token)
 			try{
-		        console.log(jwt_decode(res.token));
-		        this.authService.updateToken(res.token);
-		        this.router.navigate(['home']);
-		    }
-		    catch(Error){
-		        console.log('null');
-		    }
+				if(res.type == 'lawyer'){
+					console.log('type',res.type )
+					this.router.navigate(['/editprofile-lawyer']);
 
+				}
+				else if (res.type == 'client') {
+					console.log(jwt_decode(res.token));
+					this.authService.updateToken(res.token);
+					this.router.navigate(['home']);
+				}
+			}
+			catch(Error){
+				console.log('Something Bad Hppened');
+			}
 		},
 		(err:any)=> {
 			if(err.status == 400) {
@@ -66,5 +72,4 @@ export class LoginComponent implements OnInit {
 		}
 		);
 	}
-
 }
